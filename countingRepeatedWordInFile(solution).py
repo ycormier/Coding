40104f6688018f6a64af487eafdd986c052bc419
ycorm <y.cormier@outlook.com>
2021-06-06 20:31:26 -0600

    adding the first section of notes

--- countingRepeatedWordInFile(solution).py
+++ countingRepeatedWordInFile(solution).py
@@ -2,9 +2,15 @@ file = input("Which file would you like me to go through? >>>") + '.txt'
 if len(file) < 5:
     file = 'clown.txt'
 
-handle = open(file)
+handle = open(file) # open the file that you want to read through
+
+di = dict() # Create an empty dictionary to use later
 
 for line in handle:
-    line = line.strip()
-    word = line.split()
+    line = line.strip() # divide text into separate lines
+    word = line.split() # divide lines (above) into each word
 
+for w in word:
+    di[w] = di.get(w, 0) + 1
+
+print(di)
